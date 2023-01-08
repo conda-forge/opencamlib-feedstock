26b59c89228ff965e3c4565fd4461127aeda50f6
Koen Schmeets <hello@koenschmeets.nl>
2023-01-08 13:19:37 +0100

    Test

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,7 +1,5 @@
 export CMAKE_ARGS="${CMAKE_ARGS} -D BUILD_DOC:BOOL=OFF -D VERSION_STRING:STRING=${PKG_VERSION}"
 
 echo "CMAKE_ARGS: ${CMAKE_ARGS}"
-echo "CXX: ${CXX}"
-echo "CC: ${CC}"
 
 ${PYTHON} -m pip install . -vv
